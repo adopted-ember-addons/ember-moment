939e9795339e602b54a19b7dfbffd3d997f6f4de
Thomas Dashney <tdashney@gmail.com>
2014-12-30 04:39:16 -0500

    added second parameter option to duration helper/computed

--- addon/helpers/duration.js
+++ addon/helpers/duration.js
@@ -5,15 +5,31 @@ var duration;
 
 if (Ember.HTMLBars) {
   duration = function duration(params) {
-    if (params.length !== 0) {
-      throw new TypeError('Invalid Number of arguments, expected one argument');
+    var length = params.length;
+
+    if (length === 0 || length > 2) {
+      throw new TypeError('Invalid Number of arguments, expected 1 or 2');
     }
 
-    return moment.duration(params).humanize();
+    return moment.durationapply(this, params).humanize();
   };
 } else {
-  duration = function duration(value) {
-    return moment.duration(value).humanize();
+  duration = function duration(arg1, arg2) {
+    var length = arguments.length;
+
+    if (length === 1 || length > 3) {
+      // there's one extra argument that handlebars adds to the end,
+      // which explains the difference in what we are checking and the error we are raising
+      throw new TypeError('Invalid Number of arguments, expected 1 or 2');
+    }
+
+    var args = [arg1];
+
+    if (length === 3) {
+      args.push(arg2);
+    }
+
+    return moment.duration.apply(this, args).humanize();
   };
 }
 
