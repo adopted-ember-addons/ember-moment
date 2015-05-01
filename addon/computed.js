5eb7ddaf15c3931b954a7ea331126f95046cbe03
Eptis <dennistel90@gmail.com>
2015-05-01 11:59:49 +0200

    added old names to computed properties

--- addon/computed.js
+++ addon/computed.js
@@ -1,9 +1,15 @@
 import momentFormat from './computeds/moment-format';
 import momentAgo from './computeds/moment-ago';
 import momentDuration from './computeds/moment-duration';
+import moment from './computeds/moment';
+import ago from './computeds/ago';
+import duration from './computeds/duration';
 
 export {
-  momentFormat,
-  momentAgo,
-  momentDuration
+    moment,
+    ago
+    duration,
+    momentFormat,
+    momentAgo,
+    momentDuration
 };
