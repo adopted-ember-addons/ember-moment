3350a1546a9bbcc644334aab53f4ad5fbfb70f8a
Manuel Wiedenmann <manuel@funkensturm.de>
2015-07-08 15:45:56 +0200

    Add moment-timezone to project

--- blueprints/ember-moment/index.js
+++ blueprints/ember-moment/index.js
@@ -9,5 +9,6 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackageToProject('ember-cli-moment-shim');
+    return this.addBowerPackageToProject('moment-timezone');
   }
 };
