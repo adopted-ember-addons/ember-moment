7aa5d44255e0798c4f24d906ffa2ff66883a6b9b
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-18 13:46:59 -0700

    fixing issue where shim is a nested addon

---
+++ blueprints/ember-moment/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function() {
+    return this.addAddonToProject('ember-cli-moment-shim', '~0.3.3');
+  }
+};
