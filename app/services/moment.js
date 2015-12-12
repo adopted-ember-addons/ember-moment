5bfc34c1df51b439a412463b33dd33dfe985ad96
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-12-12 14:24:52 -0800

    Removing the need for an instance initializer

--- app/services/moment.js
+++ app/services/moment.js
@@ -1,52 +1,8 @@
-import Ember from 'ember';
-import moment from 'moment';
+import MomentService from 'ember-moment/services/moment';
+import config from '../config/environment';
 
-const { computed, Logger:logger } = Ember;
+const { get } = Ember;
 
-export default Ember.Service.extend({
-  _timeZone: null,
-
-  locale: null,
-  defaultFormat: 'LLLL',
-
-  timeZone: computed('_timeZone', {
-    get() {
-      return this.get('_timeZone');
-    },
-
-    set(propertyKey, timeZone) {
-      if (!moment.tz) {
-        logger.warn('[ember-moment] attempted to set timezone, but moment-timezone unavailable.');
-        return;
-      }
-
-      this.set('_timeZone', timeZone);
-
-      return timeZone;
-    }
-  }),
-
-  changeLocale(locale) {
-    this.set('locale', locale);
-  },
-
-  changeTimeZone(timeZone) {
-    this.set('timeZone', timeZone);
-  },
-
-  moment() {
-    let time = moment(...arguments);
-    const locale = this.get('locale');
-    const timeZone = this.get('timeZone');
-
-    if (locale) {
-      time = time.locale(locale);
-    }
-
-    if (timeZone && time.tz) {
-      time = time.tz(timeZone);
-    }
-
-    return time;
-  }
+export default MomentService.extend({
+  defaultFormat: get(config, 'moment.outputFormat')
 });
