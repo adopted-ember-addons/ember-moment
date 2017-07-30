870657a9c24fa3ac4e94a850229b66e3f1965352
Jonne Deprez <jonne.deprez@gmail.com>
2017-07-30 14:39:45 +0200

    removed lines that make Babel break

--- addon/utils/get-owner.js
+++ addon/utils/get-owner.js
@@ -4,13 +4,4 @@ import Ember from 'ember';
 
 let getOwner = Ember.getOwner;
 
-if (!getOwner) {
-  try {
-    getOwner = require('ember-getowner-polyfill')['default'];
-  }
-  catch(e) {
-    Ember.Logger.warn('Ember.getOwner API unsupported.  To resolve this: `ember install ember-getowner-polyfill`');
-  }
-}
-
 export default getOwner;
