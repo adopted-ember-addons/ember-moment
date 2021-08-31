c891c1c83065775d3a7b452e3cc77523db98cdbe
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-08-30 21:42:12 -0400

    For Later ember versions, use ember-cli-babel@^7 in CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -21,7 +21,8 @@ module.exports = {
       name: 'ember-lts-3.12',
       npm: {
         devDependencies: {
-          'ember-source': '~3.12.0'
+          'ember-source': '~3.12.0',
+          'ember-cli-babel': '^7.0.0',
         }
       }
     },
@@ -29,7 +30,8 @@ module.exports = {
       name: 'ember-lts-3.16',
       npm: {
         devDependencies: {
-          'ember-source': '~3.16.0'
+          'ember-source': '~3.16.0',
+          'ember-cli-babel': '^7.0.0',
         }
       }
     },
@@ -37,7 +39,8 @@ module.exports = {
       name: 'ember-lts-3.20',
       npm: {
         devDependencies: {
-          'ember-source': '~3.20.0'
+          'ember-source': '~3.20.0',
+          'ember-cli-babel': '^7.0.0',
         }
       }
     },
@@ -45,63 +48,10 @@ module.exports = {
       name: 'ember-lts-3.24',
       npm: {
         devDependencies: {
-          'ember-source': '~3.24.0'
+          'ember-source': '~3.24.0',
+          'ember-cli-babel': '^7.0.0',
         }
       }
     },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-default',
-      npm: {
-        devDependencies: {}
-      }
-    }
   ]
 };
