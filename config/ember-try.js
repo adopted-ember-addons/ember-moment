6792cafee97191af8bc35d402c14bc173dfd755e
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-08-30 21:06:21 -0400

    Setup GitHub Actions

--- config/ember-try.js
+++ config/ember-try.js
@@ -18,6 +18,38 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-3.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-3.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.16.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-3.20',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.20.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-3.24',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.24.0'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
