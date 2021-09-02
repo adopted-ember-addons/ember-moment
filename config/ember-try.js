d6f6975f423d9399e773a364d75af749e4331ea2
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-09-02 15:37:36 -0400

    Upgrade to 3.0 via ember-cli-update

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,57 +1,71 @@
-module.exports = {
-  useYarn: true,
-  scenarios: [
-    {
-      name: 'ember-lts-2.12',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.12.0'
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = function() {
+  return Promise.all([
+    getChannelURL('release'),
+    getChannelURL('beta'),
+    getChannelURL('canary'),
+  ]).then((urls) => {
+    return {
+      useYarn: true,
+      scenarios: [
+        {
+          name: 'ember-lts-2.12',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.12.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-2.16',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.16.0'
+            }
+          }
+        },
+        {
+          name: 'ember-lts-2.18',
+          npm: {
+            devDependencies: {
+              'ember-source': '~2.18.0'
+            }
+          }
+        },
+        {
+          name: 'ember-release',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[0]
+            }
+          }
+        },
+        {
+          name: 'ember-beta',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[1]
+            }
+          }
+        },
+        {
+          name: 'ember-canary',
+          npm: {
+            devDependencies: {
+              'ember-source': urls[2]
+            }
+          }
+        },
+        {
+          name: 'ember-default',
+          npm: {
+            devDependencies: {}
+          }
         }
-      }
-    },
-    {
-      name: 'ember-lts-2.16',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.16.0'
-        }
-      }
-    },
-    {
-      name: 'ember-lts-3.12',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.12.0',
-          'ember-cli-babel': '^7.0.0',
-        }
-      }
-    },
-    {
-      name: 'ember-lts-3.16',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.16.0',
-          'ember-cli-babel': '^7.0.0',
-        }
-      }
-    },
-    {
-      name: 'ember-lts-3.20',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.20.0',
-          'ember-cli-babel': '^7.0.0',
-        }
-      }
-    },
-    {
-      name: 'ember-lts-3.24',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.24.0',
-          'ember-cli-babel': '^7.0.0',
-        }
-      }
-    },
-  ]
+      ]
+    };
+  });
 };
