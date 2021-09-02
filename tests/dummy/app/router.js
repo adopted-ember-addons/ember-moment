bbff7653b8715621c6f77e6f2eded7e485460486
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-09-02 15:48:13 -0400

    Upgrade to 3.16 via ember-cli-update

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,14 +1,12 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-var Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
    this.route('index', { path: '/' });
    this.route('smoke', { path: '/smoke' });
 });
-
-export default Router;
