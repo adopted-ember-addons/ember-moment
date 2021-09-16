fd4919e002d954d420d2927c8cb301128b81a050
Davide Ferrero <d.ferrero@domoticalabs.com>
2021-09-16 10:45:26 +0200

    Fixes code format with prettier suggestion

--- app/services/moment.js
+++ app/services/moment.js
@@ -2,5 +2,6 @@ import MomentService from 'ember-moment/services/moment';
 import config from '../config/environment';
 
 export default class MomentOverride extends MomentService {
-  defaultFormat = (config.moment && config.moment.outputFormat) ?? 'YYYY-MM-DDTHH:mm:ssZ';
+  defaultFormat =
+    (config.moment && config.moment.outputFormat) ?? 'YYYY-MM-DDTHH:mm:ssZ';
 }
