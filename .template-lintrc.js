c385c98b7fb947305d8f3563638ff3742d668568
Edward Faulkner <edward@eaf4.com>
2022-01-17 01:26:58 -0500

    re-enable all current lint rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,9 +3,6 @@
 module.exports = {
   extends: 'octane',
   rules: {
-    // temporarily disabling these because they were already failing
-    'no-implicit-this': 'off',
-    'no-curly-component-invocation': 'off',
-    'no-action': 'off',
+    'no-curly-component-invocation': { allow: ['utc'] },
   },
 };
